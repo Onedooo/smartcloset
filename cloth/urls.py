70c7f826a59e08f05a7b64f15b6d50d8cb0c8f9c
OneDoo <92628570+Onedooo@users.noreply.github.com>
2022-06-27 09:57:52 +0900

    Update urls.py

--- cloth/urls.py
+++ cloth/urls.py
@@ -1,38 +1,10 @@
-from django.db import models
-from register.models import Account
+from django.urls import path
 
-class MyClothes(models.Model):
-    myclothid = models.IntegerField(primary_key=True)
-    accountid = models.ForeignKey(Account, on_delete=models.CASCADE, db_column='accountid')
-    mycolor = models.CharField(max_length=40)
-    mycategory = models.CharField(max_length=40)
-    buydate = models.DateField()
-    myimg = models.CharField(max_length=40)
+from . import views
 
-    def insert_cloth(self, accountid, mycolor, mycategory, buydate, myimg):
-        if not accountid:
-            raise ValueError('id를 입력하세요')
-        if not mycolor:
-            raise ValueError('color 입력하세요')
-        if not mycategory:
-            raise ValueError('category 입력하세요')
-        if not buydate:
-            raise ValueError('구매일 입력하세요')
-        if not myimg:
-            raise ValueError('이미지명을 입력하세요')
-        cloth = self.model(
-            accountid=accountid,
-            mycolor=mycolor,
-            mycategory=mycategory,
-            buydate=buydate,
-            myimg=myimg
-        )
-        cloth.save(using=self._db)
-        return cloth
-
-    class Meta:
-        managed = False
-        db_table = 'MyClothes'
-
-    def __str__(self):
-        return "이름 : " + self.name
+urlpatterns = [
+    path('save/', views.save_data),
+    path('check/<int:accountid>/', views.check_data),
+    path('edit/<int:pk>/', views.edit_data),
+    path('remove/<int:pk>/', views.remove_data),
+]
